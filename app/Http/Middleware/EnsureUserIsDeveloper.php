39def41599dbca4e0165f1a2d19fee4c18e6415c
Ikhsanndyf <117653206+Ikhsanndyf@users.noreply.github.com>
2026-07-25 22:13:04 +0700

    feat: tambahkan middleware akses developer

---
+++ app/Http/Middleware/EnsureUserIsDeveloper.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Enums\UserRole;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureUserIsDeveloper
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        abort_unless(
+            $request->user()?->role === UserRole::Developer,
+            403
+        );
+
+        return $next($request);
+    }
+}
