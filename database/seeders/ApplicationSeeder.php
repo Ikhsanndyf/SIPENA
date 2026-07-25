3ebcee5ecbec22585a36c5f5b31786195d0ca8cf
Ikhsanndyf <117653206+Ikhsanndyf@users.noreply.github.com>
2026-07-25 23:52:08 +0700

    feat: menambahkan master aplikasi internal

---
+++ database/seeders/ApplicationSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Application;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ApplicationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $applications = [
+            [
+                'name' => 'Sistem Kepegawaian',
+                'slug' => 'sistem-kepegawaian',
+            ],
+            [
+                'name' => 'Sistem Persuratan',
+                'slug' => 'sistem-persuratan',
+            ],
+            [
+                'name' => 'Dashboard Monitoring',
+                'slug' => 'dashboard-monitoring',
+            ],
+        ];
+
+        foreach ($applications as $application) {
+            Application::updateOrCreate(
+                ['slug' => $application['slug']],
+                ['name' => $application['name']],
+            );
+        }
+    }
+}
